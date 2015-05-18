dd1f0fbda56d6cff80ccbfadf5b17b0594f425df
Sharon Hershkovitz <SharonH@playscape.com>
2015-05-18 10:28:17 +0300

    show catalog. added internal class support in c# for exposing to unity developers.

---
+++ Assets/Plugins/Playscape/Internal/PlayscapeCatalogAndroid.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Playscape.Internal
+{
+	public class PlayscapeCatalogAndroid : PlayscapeCatalogBase {
+
+		#if UNITY_ANDROID
+		const string CATALOG_METHODS_CLASS_NAME = "mominis.common.PlayscapeSdk";
+	
+		private AndroidJavaClass mJavaMethodsClass = new AndroidJavaClass (CATALOG_METHODS_CLASS_NAME);
+        #endif
+
+        public override void showCatalog() {
+			#if UNITY_ANDROID
+			mJavaMethodsClass.CallStatic ("showCatalog", new object[] {});
+			#endif
+		}
+
+		
+	}
+}
---
+++ Assets/Plugins/Playscape/Internal/PlayscapeCatalogBase.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace Playscape.Internal
+{
+	public abstract class PlayscapeCatalogBase : MonoBehaviour {
+
+        public abstract void showCatalog();
+		
+
+	}
+}
