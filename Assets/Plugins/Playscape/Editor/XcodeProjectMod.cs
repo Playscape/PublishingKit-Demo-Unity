2f81f046e5eb58a3930e7c442e2d2b721c3e0909
a-shelyuk <artem@Artems-iMac.local>
2015-07-21 16:59:25 +0300

    add own patcher for Xcode project files

--- Assets/Plugins/Playscape/Editor/XcodeProjectMod.cs
+++ Assets/Plugins/Playscape/Editor/XcodeProjectMod.cs
@@ -1,17 +1,21 @@
 #if UNITY_EDITOR
 using UnityEngine;
 using UnityEditor;
+#if UNITY_5
 using UnityEditor.iOS.Xcode;
+#else 
+using UnityEditor.XCodeEditor;
+#endif
 using System.IO;
 using System.Collections;
 using System.Collections.Generic;
  
-namespace Playscape.Editor {
+namespace Playscape.Internal {
 
 	/// <summary>
 	/// Xcode project mod. Edits xcode project files
 	/// </summary>
-	public class XcodeProjectMod
+	public class XcodeProjectMod 
 	{
 		private string path;
 		private List<string> frameworks;
@@ -103,45 +107,99 @@ namespace Playscape.Editor {
 		/// </summary>
 	    public void Apply()
 	    {
-			string projPath = Path.Combine(path, "Unity-iPhone.xcodeproj/project.pbxproj");
-			PBXProject proj = new PBXProject();
-			
-			proj.ReadFromString(File.ReadAllText(projPath));
-			string target = proj.TargetGuidByName("Unity-iPhone");
+#if UNITY_5
+			ApplyWithUnity5 (path);
+#else
+			ApplyWithUnity4 (path);
+#endif
+						      
+	    }
 
+		private void ApplyWithUnity4(string path) {
+#if UNITY_4
+			XCProject proj = new XCProject (path);
+
+			PBXGroup group = proj.GetGroup(folderName);
+			PBXGroup frameworkGroup = proj.GetGroup (folderName);		
+					
+			foreach (string usrlib in libs) {
+				string completeLibPath = System.IO.Path.Combine( "usr/lib", usrlib);
+				proj.AddFile( completeLibPath, group, "SDKROOT", true, false);
+			}
+					
 			//add system frameworks to ios project
 			foreach (string framework in frameworks) {
-				proj.AddFrameworkToProject(target, framework, false);
+				string completePath = System.IO.Path.Combine( "System/Library/Frameworks", framework);
+				proj.AddFile(completePath, frameworkGroup, "SDKROOT", true, false);
 			}
 
 			//add files to project
 			string folderInProject = Path.Combine(path, folderName);
 			foreach (string file in files) {
 				string filename = Path.GetFileName(file);
-				string projFilename = Path.Combine(folderName, filename);
+				string absoluteFilePath = System.IO.Path.Combine(folderInProject, filename);
+								
+				CopyAndReplaceDirectory(file, absoluteFilePath);
 
+				if( file.EndsWith(".framework") )
+					proj.AddFile ( absoluteFilePath, frameworkGroup, "GROUP", true, false);
+				else
+					proj.AddFile ( absoluteFilePath, group);
+			}
+
+			proj.AddFrameworkSearchPaths(Path.Combine("$(SRCROOT)", folderName));
+		
+			//adding other linker flagas to ios proejct build settings
+			foreach (string flag in otherLinkerFlags) {
+				proj.AddOtherLDFlags (flag);
+			}
+
+			proj.Save ();
+#endif
+		}
+
+		private void ApplyWithUnity5(string path) {
+#if UNITY_5
+			string projPath = Path.Combine(path, "Unity-iPhone.xcodeproj/project.pbxproj");
+			PBXProject proj = new PBXProject();
+			
+			proj.ReadFromString(File.ReadAllText(projPath));
+			string target = proj.TargetGuidByName("Unity-iPhone");
+			
+			//add system frameworks to ios project
+			foreach (string framework in frameworks) {
+				proj.AddFrameworkToProject(target, framework, false);
+			}
+			
+			//add files to project
+			string folderInProject = Path.Combine(path, folderName);
+			foreach (string file in files) {
+				string filename = Path.GetFileName(file);
+				string projFilename = Path.Combine(folderName, filename);
+				
 				CopyAndReplaceDirectory(file, Path.Combine(folderInProject, filename));
 				proj.AddFileToBuild(target, proj.AddFile(projFilename, projFilename, PBXSourceTree.Source));
 			}
-
+			
 			//add framework search path
 			proj.SetBuildProperty(target, "FRAMEWORK_SEARCH_PATHS", "$(inherited)");
 			proj.AddBuildProperty(target, "FRAMEWORK_SEARCH_PATHS", string.Format("$(PROJECT_DIR)/{0}", folderName));
-
-
+			
+			
 			foreach (string usrlib in libs) {
 				string fileGuid = proj.AddFile("usr/lib/" + usrlib, string.Format("{0}/{1}", folderName, usrlib), PBXSourceTree.Sdk);
 				proj.AddFileToBuild(target, fileGuid);
 			}
-
+			
 			//adding other linker flagas to ios proejct build settings
 			foreach (string flag in otherLinkerFlags) {
 				proj.AddBuildProperty (target, "OTHER_LDFLAGS", flag);
 			}
-
+			
 			//save project
 			File.WriteAllText(projPath, proj.WriteToString());
-	    }
+#endif
+		}
 	}
 }
 #endif
